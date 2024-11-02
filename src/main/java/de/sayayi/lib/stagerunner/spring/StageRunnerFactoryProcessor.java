4587e7000f2f59f1327de8b83989f21992ede3c3
Jeroen Gremmen <jeroen.gremmen@sayayi.de>
2024-11-02 22:46:53 +0100

    move proxy bytecode generation to StageRunnerFactoryProcessor

--- src/main/java/de/sayayi/lib/stagerunner/spring/StageRunnerFactoryProcessor.java
+++ src/main/java/de/sayayi/lib/stagerunner/spring/StageRunnerFactoryProcessor.java
@@ -1,12 +1,26 @@
 package de.sayayi.lib.stagerunner.spring;
 
-import de.sayayi.lib.stagerunner.DefaultStageRunnerFactory;
-import de.sayayi.lib.stagerunner.StageFunction;
-import de.sayayi.lib.stagerunner.StageRunnerCallback;
-import de.sayayi.lib.stagerunner.StageRunnerFactory;
+import de.sayayi.lib.stagerunner.*;
 import de.sayayi.lib.stagerunner.exception.StageRunnerConfigurationException;
 import de.sayayi.lib.stagerunner.exception.StageRunnerException;
 import de.sayayi.lib.stagerunner.spring.annotation.Data;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.NamingStrategy;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.method.ParameterDescription;
+import net.bytebuddy.description.modifier.FieldManifestation;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.scaffold.InstrumentedType;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.implementation.Implementation;
+import net.bytebuddy.implementation.bytecode.*;
+import net.bytebuddy.implementation.bytecode.assign.Assigner;
+import net.bytebuddy.implementation.bytecode.assign.primitive.PrimitiveBoxingDelegate;
+import net.bytebuddy.implementation.bytecode.constant.TextConstant;
+import net.bytebuddy.implementation.bytecode.member.FieldAccess;
+import net.bytebuddy.implementation.bytecode.member.MethodInvocation;
+import net.bytebuddy.implementation.bytecode.member.MethodReturn;
+import net.bytebuddy.implementation.bytecode.member.MethodVariableAccess;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -28,11 +42,15 @@ import org.springframework.core.convert.support.DefaultConversionService;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Stream;
 
 import static java.util.Collections.emptyMap;
 import static java.util.Objects.requireNonNull;
+import static net.bytebuddy.description.modifier.Visibility.PRIVATE;
+import static net.bytebuddy.description.modifier.Visibility.PUBLIC;
+import static net.bytebuddy.dynamic.scaffold.subclass.ConstructorStrategy.Default.NO_CONSTRUCTORS;
+import static net.bytebuddy.matcher.ElementMatchers.*;
 import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
 import static org.springframework.core.ResolvableType.forClassWithGenerics;
 import static org.springframework.core.ResolvableType.forMethodParameter;
@@ -143,21 +161,39 @@ public class StageRunnerFactoryProcessor<R>
 
 
   @Contract(pure = true)
-  @SuppressWarnings("unchecked")
-  private <S extends Enum<S>> @NotNull R createStageRunnerProxy()
+  private @NotNull R createStageRunnerProxy()
   {
-    final Class<S> stageType = (Class)stageFunctionAnnotation.getStageType();
-    final Class<? extends R> stageRunnerProxyType =  new StageRunnerProxyBuilder<R,S>()
-        .createFor(stageRunnerInterfaceType, stageRunnerInterfaceMethod, stageType, dataNames,
-            copyInterfaceMethodAnnotations);
+    final Class<?> stageType = stageFunctionAnnotation.getStageType();
+    final TypeDescription.Generic stageRunnerFactoryType = TypeDescription.Generic.Builder
+        .parameterizedType(StageRunnerFactory.class, stageType)
+        .build();
+    final MethodDescription method = new MethodDescription.ForLoadedMethod(stageRunnerInterfaceMethod);
 
     try {
+      //noinspection resource
+      final Class<? extends R> stageRunnerProxyType = new ByteBuddy()
+          .with(new NamingStrategy.SuffixingRandom(stageType.getSimpleName()))
+          .subclass(stageRunnerInterfaceType, NO_CONSTRUCTORS)
+          .modifiers(PUBLIC)
+          .defineField("factory", stageRunnerFactoryType, PRIVATE, FieldManifestation.FINAL)
+          .defineConstructor(PUBLIC)
+              .withParameters(stageRunnerFactoryType)
+              .intercept(new ProxyConstructor())
+          .define(stageRunnerInterfaceMethod)
+              .intercept(new ProxyMethod(method))
+              .annotateMethod(copyInterfaceMethodAnnotations ? method.getDeclaredAnnotations() : List.of())
+          .method(isToString())
+              .intercept(FixedValue.value("Proxy for interface " + stageRunnerInterfaceType.getName()))
+          .make()
+          .load(stageRunnerInterfaceType.getClassLoader())
+          .getLoaded();
+
       return stageRunnerProxyType
-          .getDeclaredConstructor(StageRunnerFactory.class, String[].class)
-          .newInstance(stageRunnerFactory, dataNames);
+          .getDeclaredConstructor(StageRunnerFactory.class)
+          .newInstance(stageRunnerFactory);
     } catch(Exception ex) {
-      throw new StageRunnerConfigurationException("failed to instantiate proxy for stage runner interface " +
-          stageRunnerProxyType, ex);
+      throw new StageRunnerConfigurationException("failed to create proxy for stage runner interface " +
+          stageRunnerInterfaceType, ex);
     }
   }
 
@@ -168,6 +204,7 @@ public class StageRunnerFactoryProcessor<R>
     if (!interfaceType.isInterface())
       throw new StageRunnerConfigurationException(interfaceType.getName() + " is not an interface");
 
+    //noinspection ExtractMethodRecommender
     Method functionalInterfaceMethod = null;
 
     for(final Method method: interfaceType.getMethods())
@@ -292,5 +329,189 @@ public class StageRunnerFactoryProcessor<R>
 
   public void setStageFunctionBuilder(AbstractStageFunctionBuilder stageFunctionBuilder) {
     this.stageFunctionBuilder = requireNonNull(stageFunctionBuilder);
+  }
+
+
+  @Contract(pure = true)
+  private static TypeDescription typeDescription(@NotNull Class<?> type) {
+    return TypeDescription.ForLoadedType.of(type);
+  }
+
+
+
+
+  private static class ProxyConstructor implements Implementation
+  {
+    @Override
+    public @NotNull InstrumentedType prepare(@NotNull InstrumentedType instrumentedType) {
+      return instrumentedType;
+    }
+
+
+    @Override
+    public @NotNull ByteCodeAppender appender(@NotNull Target target)
+    {
+      return new ByteCodeAppender.Simple(
+          // super();
+          MethodVariableAccess.loadThis(),
+          MethodInvocation.invoke(typeDescription(Object.class)
+              .getDeclaredMethods()
+              .filter(isConstructor())
+              .getOnly()),
+          // this.factory = factory(param 1)
+          MethodVariableAccess.loadThis(),
+          MethodVariableAccess.REFERENCE.loadFrom(1),
+          FieldAccess
+              .forField(target
+                  .getInstrumentedType()
+                  .getDeclaredFields()
+                  .filter(named("factory"))
+                  .getOnly())
+              .write(),
+          // return
+          MethodReturn.VOID);
+    }
+  }
+
+
+
+
+  private class ProxyMethod implements Implementation
+  {
+    private final MethodDescription method;
+
+
+    private ProxyMethod(@NotNull MethodDescription method) {
+      this.method = method;
+    }
+
+
+    @Override
+    public @NotNull InstrumentedType prepare(@NotNull InstrumentedType instrumentedType) {
+      return instrumentedType;
+    }
+
+
+    @Override
+    public @NotNull ByteCodeAppender appender(@NotNull Target target)
+    {
+      final List<StackManipulation> stackManipulations = new ArrayList<>();
+
+      // this.factory.createRunner() -> stack
+      stackManipulations.add(MethodVariableAccess.loadThis());
+      stackManipulations.add(FieldAccess
+          .forField(target
+              .getInstrumentedType()
+              .getDeclaredFields()
+              .filter(named("factory"))
+              .getOnly())
+          .read());
+      stackManipulations.add(MethodInvocation.invoke(
+          typeDescription(StageRunnerFactory.class)
+              .getDeclaredMethods()
+              .filter(named("createRunner"))
+              .getOnly()));
+
+      // param1 = data map
+      stackManipulations.addAll(Stream.of(dataNames).anyMatch(Objects::nonNull)
+          ? buildMapWithDataNames()
+          : buildMapNoDataNames());
+
+      // param2 = callback (optional)
+      final ParameterDescription stageRunnerCallbackParameter = findCallbackParameter();
+      if (stageRunnerCallbackParameter != null)
+        stackManipulations.add(MethodVariableAccess.load(stageRunnerCallbackParameter));
+
+      stackManipulations.add(MethodInvocation.invoke(
+          typeDescription(StageRunner.class)
+              .getDeclaredMethods()
+              .filter(named("run").and(takesArguments(stageRunnerCallbackParameter != null ? 2 : 1)))
+              .getOnly()));
+
+      if (method.getReturnType().represents(void.class))
+      {
+        stackManipulations.add(Removal.SINGLE);
+        stackManipulations.add(MethodReturn.VOID);
+      }
+      else
+        stackManipulations.add(MethodReturn.INTEGER);
+
+      return new ByteCodeAppender.Simple(stackManipulations);
+    }
+
+
+    @Contract(pure = true)
+    private @NotNull List<StackManipulation> buildMapWithDataNames()
+    {
+      final List<StackManipulation> stackManipulations = new ArrayList<>();
+      final TypeDescription hashMapType = typeDescription(HashMap.class);
+
+      // new HashMap() -> stack
+      stackManipulations.add(TypeCreation.of(hashMapType));
+      stackManipulations.add(Duplication.SINGLE);
+      stackManipulations.add(MethodInvocation.invoke(hashMapType
+          .getDeclaredMethods()
+          .filter(isDefaultConstructor())
+          .getOnly()));
+
+      final MethodDescription mapPutMethod = typeDescription(Map.class)
+          .getDeclaredMethods()
+          .filter(named("put").and(takesArguments(2)))
+          .getOnly();
+      String dataName;
+
+      for(final ParameterDescription parameter: method.getParameters())
+        if ((dataName = dataNames[parameter.getIndex()]) != null)
+        {
+          stackManipulations.add(Duplication.SINGLE);
+          stackManipulations.add(new TextConstant(dataName));
+          stackManipulations.add(MethodVariableAccess.load(parameter));
+
+          final TypeDescription parameterType = parameter.getType().asErasure();
+          if (parameterType.isPrimitive())
+          {
+            stackManipulations.add(PrimitiveBoxingDelegate
+                .forPrimitive(parameterType)
+                .assignBoxedTo(
+                    TypeDescription.Generic.OfNonGenericType.ForLoadedType.of(Object.class),
+                    Assigner.DEFAULT, Assigner.Typing.STATIC));
+          }
+
+          stackManipulations.add(MethodInvocation.invoke(mapPutMethod));
+          stackManipulations.add(Removal.SINGLE);
+        }
+
+      stackManipulations.add(MethodInvocation.invoke(
+          typeDescription(Map.class)
+              .getDeclaredMethods()
+              .filter(named("copyOf"))
+              .getOnly()));
+
+      return stackManipulations;
+    }
+
+
+    @Contract(pure = true)
+    private @NotNull List<StackManipulation> buildMapNoDataNames()
+    {
+      return List.of(MethodInvocation.invoke(
+          typeDescription(Map.class)
+              .getDeclaredMethods()
+              .filter(named("of").and(takesArguments(0)))
+              .getOnly()));
+    }
+
+
+    @Contract(pure = true)
+    private ParameterDescription findCallbackParameter()
+    {
+      final TypeDescription stageRunnerCallbackType = typeDescription(StageRunnerCallback.class);
+
+      for(final ParameterDescription parameter: method.getParameters())
+        if (stageRunnerCallbackType.isAssignableFrom(parameter.getType().asErasure()))
+          return parameter;
+
+      return null;
+    }
   }
 }
