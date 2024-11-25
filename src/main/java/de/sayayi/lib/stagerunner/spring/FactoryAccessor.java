7b0160cb8a7cdcd99e37d6a392de52b13d4c56fd
Jeroen Gremmen <jeroen.gremmen@sayayi.de>
2024-11-25 21:04:07 +0100

    add javadoc

--- src/main/java/de/sayayi/lib/stagerunner/spring/FactoryAccessor.java
+++ src/main/java/de/sayayi/lib/stagerunner/spring/FactoryAccessor.java
@@ -7,19 +7,32 @@ import org.jetbrains.annotations.NotNull;
 
 
 /**
+ * Stage runner proxies may implement this interface to provide access to the internal stage runner factory.
+ *
  * @author Jeroen Gremmen
  * @since 0.3.2
  */
 @FunctionalInterface
 public interface FactoryAccessor<S extends Enum<S>>
 {
+  /**
+   * Return the populated stage runner factory.
+   *
+   * @return  stage runner factory, never {@code null}
+   */
   @Contract(pure = true)
   @NotNull StageRunnerFactory<S> getStageRunnerFactory();
 
 
+  /**
+   * Convenience method, which allows access to the stage function configurer. If the internal stage runner factory
+   * is not configurable, an {@link IllegalStateException} is thrown.
+   *
+   * @return  stage function configurer, never {@code null}
+   */
   @Contract(pure = true)
   @SuppressWarnings("unchecked")
-  default StageFunctionConfigurer<S> getStageFunctionConfigurer()
+  default @NotNull StageFunctionConfigurer<S> getStageFunctionConfigurer()
   {
     var factory = getStageRunnerFactory();
     if (factory instanceof StageFunctionConfigurer)
@@ -29,9 +42,15 @@ public interface FactoryAccessor<S extends Enum<S>>
   }
 
 
+  /**
+   * Convenience method, which allows access to the named stage function configurer. If the internal stage runner
+   * factory is not configurable, an {@link IllegalStateException} is thrown.
+   *
+   * @return  stage function configurer, never {@code null}
+   */
   @Contract(pure = true)
   @SuppressWarnings("unchecked")
-  default StageFunctionConfigurer.Named<S> getNamedStageFunctionConfigurer()
+  default @NotNull StageFunctionConfigurer.Named<S> getNamedStageFunctionConfigurer()
   {
     var factory = getStageRunnerFactory();
     if (factory instanceof StageFunctionConfigurer.Named)
