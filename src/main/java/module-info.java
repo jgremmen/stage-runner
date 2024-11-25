583e34ec93cec227284f7122df0130a78c898957
Jeroen Gremmen <jeroen.gremmen@sayayi.de>
2024-11-25 08:52:48 +0100

    add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module de.sayayi.lib.stagerunner {
+
+  // optional requirement for Spring
+  requires static net.bytebuddy;
+  requires static spring.core;
+  requires static spring.beans;
+  requires static spring.jcl;
+
+  // compile time requirement
+  requires static org.jetbrains.annotations;
+
+  // exports
+  exports de.sayayi.lib.stagerunner;
+  exports de.sayayi.lib.stagerunner.exception;
+  exports de.sayayi.lib.stagerunner.spi;
+  exports de.sayayi.lib.stagerunner.spring;
+  exports de.sayayi.lib.stagerunner.spring.annotation;
+}
